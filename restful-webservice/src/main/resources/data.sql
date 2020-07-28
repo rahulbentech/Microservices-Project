0037fe3a90b4992012320ca69fe943ff3614f8b8
R Ben <43313399+rahulbentech@users.noreply.github.com>
2020-07-28 16:32:17 +0530

    Update data.sql
    added one record

--- restful-webservice/src/main/resources/data.sql
+++ restful-webservice/src/main/resources/data.sql
@@ -4,3 +4,4 @@ insert into user values(1003, sysdate(), 'Jill');
 insert into post values(1101, 'Spring', 1001);
 insert into post values(1102, 'Rest API', 1001);
 insert into post values(1104, 'Microservices', 1002);
+insert into post values(1105, 'Hibernate', 1003);
